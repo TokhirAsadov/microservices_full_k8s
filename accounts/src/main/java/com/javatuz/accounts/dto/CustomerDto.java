35e60bd24f133673b3b394f10b06a8ab0c035370
Takhir <guvalakat1603@gmail.com>
2025-04-30 11:28:46 +0500

    CustomerDto

---
+++ accounts/src/main/java/com/javatuz/accounts/dto/CustomerDto.java
@@ -0,0 +1,14 @@
+package com.javatuz.accounts.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerDto {
+    private String name;
+    private String email;
+    private String mobileNumber;
+}
