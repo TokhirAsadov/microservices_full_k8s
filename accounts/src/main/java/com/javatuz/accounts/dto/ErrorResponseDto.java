56c15d00fcce45cb6cb10b2eb18b793d37a831f3
Takhir <guvalakat1603@gmail.com>
2025-04-30 11:29:03 +0500

    ErrorResponseDto

---
+++ accounts/src/main/java/com/javatuz/accounts/dto/ErrorResponseDto.java
@@ -0,0 +1,17 @@
+package com.javatuz.accounts.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponseDto {
+
+    private String apiPath;
+    private String errorCode;
+    private String errorMessage;
+    private LocalDateTime errorTime;
+
+}
