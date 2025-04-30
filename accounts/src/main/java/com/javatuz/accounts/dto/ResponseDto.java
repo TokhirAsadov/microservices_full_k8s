6399c4f0b4092b8ce10316d8416b624a623b410d
Takhir <guvalakat1603@gmail.com>
2025-04-30 11:28:54 +0500

    ResponseDto

---
+++ accounts/src/main/java/com/javatuz/accounts/dto/ResponseDto.java
@@ -0,0 +1,11 @@
+package com.javatuz.accounts.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResponseDto {
+    private String statusCode;
+    private String statusMsg;
+}
