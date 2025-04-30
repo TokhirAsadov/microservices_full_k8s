087648e665d6100257a08a9155b1092d128af37d
Takhir <guvalakat1603@gmail.com>
2025-04-30 12:30:24 +0500

    createAccount()

--- accounts/src/main/java/com/javatuz/accounts/service/IAccountsService.java
+++ accounts/src/main/java/com/javatuz/accounts/service/IAccountsService.java
@@ -1,4 +1,10 @@
 package com.javatuz.accounts.service;
 
+import com.javatuz.accounts.dto.CustomerDto;
+
 public interface IAccountsService {
+    /**
+     * @param customerDto - CustomerDto object
+     * */
+    void createAccount(CustomerDto customerDto);
 }
