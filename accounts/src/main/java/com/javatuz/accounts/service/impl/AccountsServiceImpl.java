92b376ebec6e3fd695fae559a9397b9573ab22e2
Takhir <guvalakat1603@gmail.com>
2025-04-30 11:58:16 +0500

    AccountsServiceImpl

---
+++ accounts/src/main/java/com/javatuz/accounts/service/impl/AccountsServiceImpl.java
@@ -0,0 +1,8 @@
+package com.javatuz.accounts.service.impl;
+
+import com.javatuz.accounts.service.IAccountsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountsServiceImpl implements IAccountsService {
+}
