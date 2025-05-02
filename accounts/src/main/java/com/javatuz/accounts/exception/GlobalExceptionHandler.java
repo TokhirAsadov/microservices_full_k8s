3caf494f4d610078a254f1198fa7fbcebfc0fab0
Takhir <guvalakat1603@gmail.com>
2025-05-02 10:31:57 +0500

    handleCustomerAlreadyExistsException()

--- accounts/src/main/java/com/javatuz/accounts/exception/GlobalExceptionHandler.java
+++ accounts/src/main/java/com/javatuz/accounts/exception/GlobalExceptionHandler.java
@@ -1,7 +1,25 @@
 package com.javatuz.accounts.exception;
 
+import com.javatuz.accounts.dto.ErrorResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomerAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponseDto> handleCustomerAlreadyExistsException(CustomerAlreadyExistsException ex, WebRequest webRequest) {
+        ErrorResponseDto errorResponse = new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.BAD_REQUEST,
+                ex.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
 }
