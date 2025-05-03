137866d0fe3e2b3a65ca867935fbf44743a047c3
Takhir <guvalakat1603@gmail.com>
2025-05-03 12:11:39 +0500

    removing

--- accounts/src/main/java/com/javatuz/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/javatuz/accounts/service/impl/AccountsServiceImpl.java
@@ -34,8 +34,8 @@ public class AccountsServiceImpl implements IAccountsService {
                     "Customer with mobile number " + customer.getMobileNumber() + " already exists."
             );
         }
-        customer.setCreatedAt(LocalDateTime.now());
-        customer.setCreatedBy("Anonymous");
+//        customer.setCreatedAt(LocalDateTime.now());
+//        customer.setCreatedBy("Anonymous");
         Customer savedCustomer = customerRepository.save(customer);
         accountsRepository.save(createNewAccount(savedCustomer));
     }
@@ -93,8 +93,8 @@ public class AccountsServiceImpl implements IAccountsService {
         newAccount.setAccountNumber(randomAccNumber);
         newAccount.setAccountType(AccountsConstants.SAVINGS);
         newAccount.setBranchAddress(AccountsConstants.ADDRESS);
-        newAccount.setCreatedAt(LocalDateTime.now());
-        newAccount.setCreatedBy("Anonymous");
+//        newAccount.setCreatedAt(LocalDateTime.now());
+//        newAccount.setCreatedBy("Anonymous");
         return newAccount;
     }
 }
