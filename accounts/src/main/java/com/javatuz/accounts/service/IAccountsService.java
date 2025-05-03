f083d813d16f1b6f2649df0ce854d4ab376cdc50
Takhir <guvalakat1603@gmail.com>
2025-05-03 10:57:47 +0500

    updateAccount()

--- accounts/src/main/java/com/javatuz/accounts/service/IAccountsService.java
+++ accounts/src/main/java/com/javatuz/accounts/service/IAccountsService.java
@@ -9,4 +9,6 @@ public interface IAccountsService {
     void createAccount(CustomerDto customerDto);
 
     CustomerDto fetchAccount(String mobileNumber);
+
+    Boolean updateAccount(CustomerDto customerDto);
 }
