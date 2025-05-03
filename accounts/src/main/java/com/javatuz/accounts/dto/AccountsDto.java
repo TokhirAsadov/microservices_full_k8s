daee68e086b1e6b04aec664665953c2e935c54f4
Takhir <guvalakat1603@gmail.com>
2025-05-03 11:40:07 +0500

    @NotEmpty() @Pattern()

--- accounts/src/main/java/com/javatuz/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/javatuz/accounts/dto/AccountsDto.java
@@ -1,5 +1,7 @@
 package com.javatuz.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AccountsDto {
+
+    @NotEmpty(message = "AccountNumber cannot be a null or empty")
+    @Pattern(regexp = "(^S|[0-9]{10})",message = "Mobile number must be 10 digits")
     private Long accountNumber;
+
+    @NotEmpty(message = "AccountType cannot be a null or empty")
     private String accountType;
+
+    @NotEmpty(message = "BranchAddress cannot be a null or empty")
     private String branchAddress;
 }
--- accounts/src/main/java/com/javatuz/accounts/dto/CustomerDto.java
+++ accounts/src/main/java/com/javatuz/accounts/dto/CustomerDto.java
@@ -1,5 +1,9 @@
 package com.javatuz.accounts.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +12,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CustomerDto {
+
+    @NotEmpty(message = "Name cannot be a null or empty")
+    @Size(min = 5, max = 30, message = "The length of the customer name should be between 5 and 30")
     private String name;
+
+    @NotEmpty(message = "Email address cannot be a null or empty")
+    @Email(message = "Email address should be a valid value")
     private String email;
+
+    @Pattern(regexp = "(^S|[0-9]{9})",message = "Mobile number must be 9 digits")
     private String mobileNumber;
 
     private AccountsDto accountsDto;
