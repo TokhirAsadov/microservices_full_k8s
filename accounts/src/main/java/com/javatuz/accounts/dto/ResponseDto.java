3fc682d50528ff32f4219cfe0a0ba1eeb061b1ea
Takhir <guvalakat1603@gmail.com>
2025-05-05 11:01:12 +0500

    @Schema()

--- accounts/src/main/java/com/javatuz/accounts/dto/ResponseDto.java
+++ accounts/src/main/java/com/javatuz/accounts/dto/ResponseDto.java
@@ -1,5 +1,6 @@
 package com.javatuz.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +8,19 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(
+        name = "Response",
+        description = "Schema to hold successful response information"
+)
 public class ResponseDto {
+
+    @Schema(
+            description = "Status code in the response", example = "200"
+    )
     private String statusCode;
+
+    @Schema(
+            description = "Status message in the response", example = "Request processed successfully"
+    )
     private String statusMsg;
 }
