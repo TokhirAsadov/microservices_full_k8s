cb3b1de4e54c4c63801cedb803749857372d7cfb
Takhir <guvalakat1603@gmail.com>
2025-05-22 13:20:44 +0500

    constants

--- section_3/v1-springboot/accounts/src/main/java/com/javatuz/accounts/constants/AccountsConstants.java
+++ section_3/v1-springboot/accounts/src/main/java/com/javatuz/accounts/constants/AccountsConstants.java
@@ -13,4 +13,7 @@ public class AccountsConstants {
     public static final String MESSAGE_200 = "Request processed successfully";
     public static final String STATUS_500 = "500";
     public static final String MESSAGE_500 = "An error occurred. Please try again later or contact support.";
+    public static final String STATUS_417 = "417";
+    public static final String MESSAGE_417_UPDATE = "Update operation failed. Please try again later or contact support.";
+    public static final String MESSAGE_417_DELETE = "Delete operation failed. Please try again later or contact support.";
 }
