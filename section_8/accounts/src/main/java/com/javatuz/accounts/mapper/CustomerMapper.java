7b03e571ca4dad3fbafff8e997492a1170fc4bea
Takhir <guvalakat1603@gmail.com>
2025-09-08 10:59:17 +0500

    mapToCustomerDetailsDto

--- section_8/accounts/src/main/java/com/javatuz/accounts/mapper/CustomerMapper.java
+++ section_8/accounts/src/main/java/com/javatuz/accounts/mapper/CustomerMapper.java
@@ -1,5 +1,6 @@
 package com.javatuz.accounts.mapper;
 
+import com.javatuz.accounts.dto.CustomerDetailsDto;
 import com.javatuz.accounts.dto.CustomerDto;
 import com.javatuz.accounts.entity.Customer;
 
@@ -17,6 +18,13 @@ public class CustomerMapper {
         customer.setEmail(customerDto.getEmail());
         customer.setMobileNumber(customerDto.getMobileNumber());
         return customer;
+    }
+
+    public static CustomerDetailsDto mapToCustomerDetailsDto(Customer customer, CustomerDetailsDto customerDetailsDto) {
+        customerDetailsDto.setName(customer.getName());
+        customerDetailsDto.setEmail(customer.getEmail());
+        customerDetailsDto.setMobileNumber(customer.getMobileNumber());
+        return customerDetailsDto;
     }
 
 }
