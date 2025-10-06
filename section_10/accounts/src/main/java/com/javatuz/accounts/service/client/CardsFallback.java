669a55d5153d120e5b44d9a72a3adab19149ba50
Takhir <guvalakat1603@gmail.com>
2025-10-06 12:09:48 +0500

    circuitbreaker

--- section_10/accounts/src/main/java/com/javatuz/accounts/service/client/CardsFallback.java
+++ section_10/accounts/src/main/java/com/javatuz/accounts/service/client/CardsFallback.java
@@ -1,7 +1,6 @@
 package com.javatuz.accounts.service.client;
 
 import com.javatuz.accounts.dto.CardsDto;
-import com.javatuz.accounts.dto.LoansDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
