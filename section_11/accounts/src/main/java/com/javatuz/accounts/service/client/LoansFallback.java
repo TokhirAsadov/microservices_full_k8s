6992acb3b9bcc20bd8db2a3d47398593567d2266
Takhir <guvalakat1603@gmail.com>
2025-10-21 11:08:46 +0500

    section 11 is beginning.

---
+++ section_11/accounts/src/main/java/com/javatuz/accounts/service/client/LoansFallback.java
@@ -0,0 +1,15 @@
+package com.javatuz.accounts.service.client;
+
+import com.javatuz.accounts.dto.LoansDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoansFallback implements LoansFeignClient{
+
+    @Override
+    public ResponseEntity<LoansDto> fetchLoansDetails(String correlationId, String mobileNumber) {
+        return null;
+    }
+
+}
---
+++ section_11/accounts/src/main/java/com/javatuz/accounts/service/client/LoansFeignClient.java
@@ -0,0 +1,19 @@
+package com.javatuz.accounts.service.client;
+
+import com.javatuz.accounts.dto.LoansDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * default holatda 30s eureka dagi loans serverga ulanishni kutadi
+ * */
+@FeignClient(name = "loans", fallback = LoansFallback.class)
+public interface LoansFeignClient {
+
+    @GetMapping(value = "/api/fetch",consumes = "application/json")
+    ResponseEntity<LoansDto> fetchLoansDetails(@RequestHeader(value = "eazybank-correlation-id") String correlationId, @RequestParam String mobileNumber);
+
+}
